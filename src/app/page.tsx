5b6fbe311b4ffacdab34cb8a1128c86cd19d0c3c
billy.berkouwer <billy@ocove.studio>
2024-03-28 14:20:19 +0000

    update: alter home metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Homepage from "@/components/home/Homepage"
 
 export const metadata = {
-  title: 'Homepage | Web Dev & 3D | Billy Myles-Berkouwer',
+  title: 'Web Dev & 3D | Billy Myles-Berkouwer',
   description: 'Website for the web development & 3D practice of Billy Myles-Berkouwer',
 }
 
