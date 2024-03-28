3beb61f6e12ece52ca1045fb134b73ddff8f6345
billy.berkouwer <billy@ocove.studio>
2024-03-28 18:41:31 +0000

    update: sitemap with priority

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,6 +4,8 @@ export default async function sitemap() {
   const routes = ["", "info"].map((route) => ({
     url: `${process.env.SITE_URL}/${route}`,
     lastModified: new Date().toISOString(),
+    changeFrequence: 'monthly',
+    priority: route === "" ? 1 : 0.9,
   }));
 
   const allProjectSlugs = await getAllProjectSlugs();
@@ -11,6 +13,8 @@ export default async function sitemap() {
   const projectRoutes = allProjectSlugs.map((projectSlug: any) => ({
     url: `${process.env.SITE_URL}/${projectSlug}`,
     lastModified: new Date().toISOString(),
+    changeFrequence: 'yearly',
+    priority: projectSlug === "i-am-everything-you-want-me-to-be" ? 0.8 : 0.6
   }));
 
   return [
