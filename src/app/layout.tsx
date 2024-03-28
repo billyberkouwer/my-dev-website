18ff8a9e6939d9e6b6b2ef5a7d9af0cba476a041
billy.berkouwer <billy@ocove.studio>
2024-03-28 17:36:34 +0000

    update: configure metabase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,11 @@ import "@/styles/global.scss";
 import { Mondwest } from "./font/font";
 import Layout from "@/components/layout/Layout";
 
+const url = process.env.SITE_URL as string;
+
 export const metadata: Metadata = {
   title: "Web Development & 3D | Billy Myles-Berkouwer",
+  metadataBase: new URL(url),
 };
 
 export default function RootLayout({
