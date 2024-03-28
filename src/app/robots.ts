2f571c520176c6332332ac1424990b638a9e12bf
billy.berkouwer <billy@ocove.studio>
2024-03-28 14:08:27 +0000

    update: robots ts

---
+++ src/app/robots.ts
@@ -0,0 +1,13 @@
+const baseUrl = process.env.SITE_URL;
+
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: "*",
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
+  };
+}
