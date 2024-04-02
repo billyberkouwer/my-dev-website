d2715ee75c859d4c14c4c5044bb96a4290513ddc
billy.berkouwer <billy@ocove.studio>
2024-04-02 02:00:42 +0100

    change meta description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Homepage from "@/components/home/Homepage"
 
 export const metadata = {
-  title: 'Web Dev & 3D | Billy Myles-Berkouwer',
-  description: 'Website for the web development & 3D practice of Billy Myles-Berkouwer',
+  title: 'Web Development & 3D - Billy Myles-Berkouwer',
+  description: 'Billy Myles-Berkouwer is a London-based full-stack web developer, artist, and 2D & 3D designer. He has worked freelance as lead developer on a variety of web projects and has particular expertise in developing browser-based 3D experiences.  ',
 }
 
 export default function Home() {
